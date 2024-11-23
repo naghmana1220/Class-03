12d600326634821e8c44cdda82b5e8761eb293ba
naghmana1220 <naghmanaasif85@gmail.com>
2024-11-23 18:40:58 +0500

    first commit

---
+++ my-work/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return (
+        <div className="bg-gray-900 text-white h-10 mt-20 font-serif flex items-center justify-center">
+             Created with love Naghmana
+             
+        </div>
+    
+    )
+}
---
+++ my-work/app/page.tsx
@@ -0,0 +1,16 @@
+
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+   
+  <div>
+    <Footer />
+    <div className="bg-gray-600 font-serif text-white flex items-center justify-center">
+        Education Governor Sindh It Initiative
+     </div>
+  </div>
+        
+         
+  );
+}
